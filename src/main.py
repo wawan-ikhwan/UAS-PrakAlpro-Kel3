914bfe89ce03b760efff19fcd2799dc8aa68d4cc
wawan-ikhwan <ikhwanperwira@gmail.com>
2021-12-15 06:12:07 +0700

    add string sorting support

--- src/main.py
+++ src/main.py
@@ -12,19 +12,22 @@ df = pd.read_csv(DIR+'/./data/input/dataset_superstore_simple.csv') # Read lokas
 def sortBy(dataframe,kolom,urutan='naik',simpan=False):
   result=dataframe.copy()
   sortedList=InsertionSort(result[kolom].tolist(),kolom,urutan)
-  if urutan != 'naik': sortedList=sortedList[::-1]
+
+  # Reindexing...
   result['g'] = result.groupby(kolom).cumcount()
   df2 = pd.DataFrame({kolom: sortedList})
   df2['g'] = df2.groupby(kolom).cumcount()
-  result=result.set_index([kolom, 'g']).reindex(df2.set_index([kolom,'g']).index).reset_index().drop('g',axis=1)
+  result=result.set_index([kolom, 'g']).reindex(df2.set_index([kolom,'g']).index).reset_index().drop('g',axis=1).reindex(columns=result.columns.tolist())
+
   if simpan:
-    result.to_csv(DIR+'/./data/output/sorted/'+kolom+'.csv')
+    result.to_csv(DIR+'/./data/output/sorted/'+kolom+'-'+urutan+'.csv',index=False)
   return result
 
 def searchBy(dataframe,kolom,cari):
   return LinearSearch(dataframe[kolom],cari) # Mengembalikan indeks
 #=========================================
-sortedDF=sortBy(df,'sales',urutan='turun',simpan=True)
+
+sortedDF=sortBy(df,'order_date',urutan='turun',simpan=True)
 print(sortedDF)
 atIndex=searchBy(df,'sales',258.576)
 print(atIndex)
